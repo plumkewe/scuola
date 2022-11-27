e2bcc76f00d85efc437f74f60590e3e59dd9f5a8
plumpkekes <113628928+plumkewe@users.noreply.github.com>
2022-11-27 15:05:53 +0100

    Esercizi dal libro

---
+++ Esercizi dal libro/pag67.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+//  
+
+
+int main() {
+	setlocale(LC_ALL, "italian");
+	
+	int dis;
+	
+	cout << "Inserisci distanza: ";
+	cin >> dis;
+	
+	cout << "Hai inserito distanza pari a " << dis;
+	
+	
+	return 0;
+}
