6134cf0ad1e4e7cda6a3426d8d92af14419887e5
plumpkekes <113628928+plumkewe@users.noreply.github.com>
2022-11-30 22:12:30 +0100

    es1

---
+++ Esercizi dal libro/Esercizi dal libro/es1pag122.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+//  Creato: 30/11/22,
+//  Da: swampkewe
+
+void crescente (int a, int b) {
+
+    if (a > b)
+        cout << b << " " << a;
+    else
+        cout << a << " " << b;
+}
+
+
+int main() {
+    setlocale(LC_ALL, "italian");
+
+    int a, b;
+
+    cout << "inserisci i due numeri: ";
+    cin >> a >> b;
+
+    crescente(a, b);
+
+    return 0;
+}
+
+//  Scrivi un algoritmo e relativo codice in C++ che, dati in input due numeri, li scriva in ordine crescente.
